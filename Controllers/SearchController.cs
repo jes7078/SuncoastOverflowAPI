99393a04ab9ef8b4314e0633620803da0cff1852
Jeffrey Sojack <jeffrey.sojack@gmail.com>
2020-01-09 13:24:51 -0500

    added search controller

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SuncoastOverflowAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuncoastOverflowAPI.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class SearchController: ControllerBase
+  {
+    private readonly DatabaseContext db;
+
+    public SearchController(DatabaseContext context)
+    {
+      this.db=context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> SearchQuestions([FromQuery]string searchTerm)
+    {
+      var results=db.Questions.Where(question=>question.Title.ToLower().Contains(searchTerm.ToLower()) ||
+      question.Description.ToLower().Contains(searchTerm.ToLower()) ||
+      question.Body.ToLower().Contains(searchTerm.ToLower()));
+      var query=new SearchQuery
+      {
+        SearchTerm = searchTerm
+      };
+      db.SearchQueries.Add(query);
+      await db.SaveChangesAsync();
+      return Ok(results);
+    }
+
+    [HttpGet("queries")]
+    public async Task<ActionResult> GetRecentSearchQueries()
+    {
+      var queries = db.SearchQueries.OrderByDescending(o=>o.TimeStamp).Take(10);
+      return Ok(queries);
+    }
+  }
+}
